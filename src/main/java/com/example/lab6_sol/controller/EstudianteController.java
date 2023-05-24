8b9f1ed4388e237c4822de0a4caf1fd690e8fbf4
CarlosPisco <edupiscote.cept@gmail.com>
2023-05-23 21:53:24 -0500

    avance lab 6

--- src/main/java/com/example/lab6_sol/controller/EstudianteController.java
+++ src/main/java/com/example/lab6_sol/controller/EstudianteController.java
@@ -2,13 +2,16 @@ package com.example.lab6_sol.controller;
 
 import com.example.lab6_sol.entity.Usuario;
 import com.example.lab6_sol.repository.UsuarioRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/estudiante")
@@ -23,4 +26,50 @@ public class EstudianteController {
         model.addAttribute("estudiantes", estudiantes);
         return "lista_usuarios";
     }
+
+    @GetMapping("/edit")
+    public String editarEst (@RequestParam("id") Integer id, @ModelAttribute ("usuario") Usuario usuario,Model model){
+        Boolean nuevo = false;
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+
+        if(usuarioOptional.isPresent()){
+            usuario = usuarioOptional.get();
+            model.addAttribute("usuario",usuario);
+            model.addAttribute("nuevo",nuevo);
+            return "formEst";
+        }else{
+            return "redirect:/estudiante/lista";
+        }
+
+
+    }
+
+    @GetMapping("/new")
+    public String nuevoEst (@ModelAttribute ("usuario") Usuario usuario, Model model){
+        Boolean nuevo = true;
+        model.addAttribute("nuevo",nuevo);
+        return "formEst";
+
+    }
+
+
+    @PostMapping("/save")
+    public String guardarEst(@ModelAttribute ("usuario") @Valid Usuario usuario, BindingResult bindingResult, RedirectAttributes attr){
+
+        if(bindingResult.hasErrors()){
+            return "formEst";
+        }else{
+            if(usuario.getId()==0){
+                attr.addFlashAttribute("msg", "Usuario creado exitosamente");
+            }else{
+                attr.addFlashAttribute("msg", "Usuario actualizado exitosamente");
+            }
+            usuarioRepository.save(usuario);
+            return "redirect:/estudiante/lista";
+        }
+
+
+    }
+
+
 }
--- src/main/java/com/example/lab6_sol/entity/Usuario.java
+++ src/main/java/com/example/lab6_sol/entity/Usuario.java
@@ -1,6 +1,9 @@
 package com.example.lab6_sol.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "usuario")
@@ -11,22 +14,39 @@ public class Usuario {
     @Column(nullable = false)
     private int id;
 
+    @Size(max=100,message="El nombre no puede tener mas de 100 caracteres")
+    @Size(min=3,message="El nombre no puede tener menos de 3 caracteres")
     @Column(nullable = false)
+    @NotBlank
     private String nombres;
 
+    @Size(max=100,message="El apellido no puede tener mas de 100 caracteres")
+    @Size(min=3,message="El apellido no puede tener menos de 3 caracteres")
     @Column(nullable = false)
+    @NotBlank
     private String apellidos;
 
+
+    @Size(max=8,message="El dni tiene 8 caracterees")
+    @Size(min=8,message="El dni tiene 8 caracterees")
     @Column(nullable = false)
+    @NotBlank
     private String dni;
 
+
+    @Min(value=0)
     @Column(nullable = false)
+    @NotBlank
     private int edad;
 
-    @Column(nullable = false)
-    private String correo;
 
     @Column(nullable = false)
+    @NotBlank
+    private String correo;
+
+
+    @Column(nullable = false)
+    @NotBlank
     private String password;
 
     @Column(nullable = false)
